639f97a9f6382a0a31ba340dcd8ab2dc4c1fa130
PickUpThisNickName <109733400+PickUpThisNickName@users.noreply.github.com>
2023-05-27 19:10:11 +0500

    header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom"
 export const Header = () => {
     return(
         <>
-            <div className={"container mx-auto flex flex-row px-16 justify-between py-6"}>
-                <Link to={'/'} className="text-center">Промоботы</Link>
-                <Link to={'/'} className="text-center">Заявки</Link>
-                <Link to={'/'} className="text-center">Администрирование</Link>
+            <div className={"container mx-auto flex flex-row p-6"}>
+                <div className={"container flex flex-row basis-1/2 justify-end"}>
+                    <Link to={'/products'} className="text-center border-2 border-black p-2 rounded w-24 md:w-40">Промоботы</Link>
+                </div>
+                <div className={"container flex flex-row basis-1/2 justify-between ml-10"}>
+                    <Link to={'/servicetickets'} className="text-center border-2 border-black p-2 rounded w-24 md:w-40">Заявки</Link>
+                    <Link to={'/login'} className="text-center border-2 border-black p-2 rounded ml-2">Администрирование</Link>
+                </div>
             </div>
         </>
     )
